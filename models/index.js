9ad1487496d4c62072ec6157a64f79c8dbbd1248
Ann Layman <annelayman@gmail.com>
2017-11-10 16:37:24 -0500

    Set up tables via sequelize

---
+++ models/index.js
@@ -0,0 +1,28 @@
+var Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING
+    },
+    urlTitle: {
+        type: Sequelize.STRING
+    },
+    content: {
+        type: Sequelize.TEXT
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    }
+});
+
+var User = db.define('user', {
+    name: {
+        type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+});
+
+module.exports = {
+  Page: Page,
+  User: User
+}; 
