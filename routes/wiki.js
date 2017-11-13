46adcdda98ca9e8a0da2d605d061a381df645fe8
Mimi Scott <mimiscott94@gmail.com>
2017-11-13 10:27:18 -0500

    set up routers

---
+++ routes/wiki.js
@@ -0,0 +1,21 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+router.get('/wiki/', function(req, res, next){
+
+})
+
+router.post('/wiki/', function(req, res, next){
+    
+})
+
+router.get('/wiki/add/', function(req, res, next){
+    
+})
+
+
+
+
+
+module.exports = router;
